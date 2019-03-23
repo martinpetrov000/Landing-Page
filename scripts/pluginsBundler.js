7694add397b255c03aeecdbbb1e2333fc212839d
martinpetrov000 <martin.petrov033@gmail.com>
2019-03-23 01:30:51 -0700

    add slick jquery plugin

---
+++ scripts/pluginsBundler.js
@@ -0,0 +1,9 @@
+//config jquery slider
+$(document).on('ready', function() {      
+    $(".regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+    });
+});
